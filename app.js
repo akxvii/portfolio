b9f03ad3a2bcfb60915897cf4221acf354bae151
akxvii <akhil03.x@gmail.com>
2025-08-13 16:19:02 +0530

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log(document.querySelector(".navbar"));
+console.log(document.querySelector(".logo"));
+console.log(document.querySelector(".add-first").innerText);
+console.log(document.querySelector(".add-second").innerText);
+console.log(document.querySelector(".nav-second").innerText);
